6e484073e13291f49aec0bdbb5686e663b26256c
Aditya Kumar <aditya.kumar@autoplant.in>
2024-07-01 16:26:08 +0530

    Updated Style

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = () => {
     return (
         <div className="flex-grow p-5 flex flex-wrap justify-center items-center gap-10 overflow-auto">
             {gameList && gameList.map((item) => (
-                <div key={item.id} className="w-60 border-solid border-2 border-indigo-600 mb-4 mx-4">
+                <div key={item.id} className="w-60 border-solid border-2 border-indigo-600 transform transition duration-300 ease-in-out hover:scale-110 cursor-pointer">
                     <img src={imzUrl[item.imzname]} alt="Tick Tak Toe" className="w-60 h-60 mx-auto" />
                     <div className="bg-neutral-400 text-xl font-bold text-white p-3 flex justify-center">{item.gamename}</div>
                 </div>
